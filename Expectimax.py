c3cc065845d567754c268b5cb6c2e4cef7ceb4d4
Jperez65 <37123419+Jperez65@users.noreply.github.com>
2020-11-23 14:46:51 -0500

    Add files via upload

--- Expectimax.py
+++ Expectimax.py
@@ -1,11 +1,14 @@
 #
 # Expectimax Algorithm Searches for the best 
 # 
-import game, math, random
+import game, math, sys, random
 
-#function 
+#functions
+
+#Function will loop through a single row 
 def check_row(board, row, column):
 
+    
     for j in range(column+1, 4):
         if(board[row][j]!=0 and board[row][column]!=board[row][j]):
             return False
@@ -25,20 +28,49 @@ def weight_evaluation(board):
     weight_grid= []
 
     #create a weighted board to determine value positioning on the board
-    for i in range(4):
-        weight_grid.append([0]*4)
-        
-        for j in range(4):
-            weight_grid[i][j]=pow(4, 6-(j))
-        
-        weight_val-=1
+    weight_grid=[[30, 15,5,3],[15,5,3,1],[5,3,1,0],[3,1,0,0]]
+
+    #montonic heuristic 
     #loop through the board and determine it value by its position and the value of the key
     for i in range(3):
         for j in range(3):
-            if(board[i][j]!=0 and (check_row(board, i, j) or check_column(board, i, j)) ):
-            #board[i+1][j] == board[i][j] or board[i][j+1] ==board[i][j])
-                var+=weight_grid[i][j]* (board[i][j]*2)
-    return var
+            if(board[i][j]!=0):        #and (check_row(board, i, j) or check_column(board, i, j)) ):
+                if(board[i][j+1]!=0 or board[i+1][j]!=0):
+                    if(board[i+1][j] == board[i][j] or board[i][j+1] ==board[i][j]): 
+                        var+=weight_grid[i][j]+(board[i][j]*2)
+                    elif(board[i][j+1]<board[i][j] or board[i+1][j] < board[i][j]):
+                        var+=weight_grid[i][j]+(board[i][j]*6)
+                                        #board[i+1][j] == board[i][j] or board[i][j+1] ==board[i][j])
+                else:
+                    var+=weight_grid[i][j]+ (board[i][j])
+
+    for i in range(3):
+        if(board[i][3]) != 0:
+            if board[i+1][3] != 0:
+                if(board[i+1][3] < board[i][3]):
+                    var+=weight_grid[i][3]+(board[i][3]*6)
+                elif (board[i+1][3]==board[i][3]):
+                    var+=weight_grid[i][3]+(board[i][3]*2)
+            else:
+                var+=weight_grid[i][3]+(board[i][3])
+
+    for j in range(3):
+        if(board[3][j]) != 0:
+            if board[3][j+1] != 0:
+                if(board[3][j+1] < board[3][j+1]):
+                    var+=weight_grid[3][j]+(board[3][j]*6)
+                elif (board[3][j+1] == board[3][j]):
+                    var+=weight_grid[i][3]+(board[i][3]*2)
+            else : 
+                var+=weight_grid[3][j]+(board[3][j])
+
+    penalty= 0
+    for i in range(4):
+        value=board[i][0]
+        for j in range(4):
+            penalty+= abs(board[i][j] - value)+weight_grid[i][j]
+    #print("Penalty: ", penalty)
+    return var -penalty
 
 def add_key():
     x = random.randint(2,4)
@@ -71,16 +103,16 @@ def decision(board, action):
 
     if action == 0:
         board, temp=game.move_up(board)
-        return board
+        return board, temp
     elif action == 1:
         board, temp = game.move_down(board)
-        return board
+        return board, temp
     elif action == 2:
         board, temp = game.move_right(board)
-        return board
+        return board, temp
     elif action ==3:
         board, temp = game.move_left(board)
-        return board 
+        return board, temp
 
 # main Fucntions that are use to find the Expectimax
 
@@ -88,7 +120,7 @@ def decision(board, action):
 def expectiminimax(board, node_type, depth):
 
     if game.get_current_board(board) == "Won":
-        return 100000
+        return sys.maxsize
 
     elif depth == 4:
         return weight_evaluation(board)
@@ -102,9 +134,12 @@ def expectiminimax(board, node_type, depth):
         
         for i in range(4):
             new_board= copy_board(board)
-            decision(new_board, i)
+            new_board, flag =decision(new_board, i)
+            #if flag:
             a = max(a, expectiminimax(new_board, "chance", depth+1) )
-    
+            #else:
+            #    continue
+
     elif node_type == "chance":
         #print("hit for chance")
         a = 0
@@ -115,13 +150,25 @@ def expectiminimax(board, node_type, depth):
                 if board[i][j]==0:
 
                     new_board = copy_board(board)
-                    new_board[i][j] = add_key()
-
+                    new_board[i][j] = 2
                     count+=1
-                    a = a + expectiminimax(new_board, "max", depth +1) 
-        if count != 0:
-            a = expectiminimax(board,"max" ,depth +1)
 
+                    result=expectiminimax(new_board, "max", depth +1)
+
+                    new_board[i][j] = 4
+                    result2=expectiminimax(new_board, "max", depth +1)
+
+                    if result> result2:
+                        a+=result
+                    else:
+                        a+=result2
+
+        if count == 0:
+            a = expectiminimax(board,"max" ,depth +1)
+        #else :
+        #   a = a/count
+            
+    #print("Score at depth: ", depth, " = ", a)
     return a
 
 def ai_expectimax(board):
@@ -132,12 +179,19 @@ def ai_expectimax(board):
 
     for i in range(4):
             new_board= copy_board(board)
-            new_board = decision(new_board, i)
-            a = max(a, expectiminimax(new_board, "chance", 0+1) )
+            new_board, flag  = decision(new_board, i)
+            if flag:
+                result = expectiminimax(new_board, "chance", 0+1)
+                a = max(a, result)
+                
+                #game.print_board(new_board)
+                #print(result)
+                decision_result.append([result, i])
+            else:
+                continue
 
-            decision_result.append(a)
-
-    print(decision_result)
+   # print(decision_result)
     for i in range(len(decision_result)):
-        if a == decision_result[i]:
-            return i
+        if a == decision_result[i][0]:
+            return decision_result[i][1]
+    return None
